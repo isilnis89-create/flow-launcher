4aaa8567ee9d9bec2de01bca6340f712c2251575
isilnis89-create <isilnis89@gmail.com>
2026-09-20 19:34:37 +0200

    Add favorite customization model

---
+++ app/src/main/java/com/nick/flowlauncher/FavoriteOverride.java
@@ -0,0 +1,13 @@
+package com.nick.flowlauncher;
+
+import android.graphics.Bitmap;
+
+public final class FavoriteOverride {
+    public final String name;
+    public final Bitmap icon;
+
+    public FavoriteOverride(String name, Bitmap icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+}
